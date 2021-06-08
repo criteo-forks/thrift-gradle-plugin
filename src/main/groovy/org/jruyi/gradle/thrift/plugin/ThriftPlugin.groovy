f2f35d678fb4ac7fe3fed8f7c8430fda4faf57b2
Bo HOU <b.hou@criteo.com>
2021-06-08 14:57:19 +0200

    Laze resolve the configuration dependencies
    Change-Id: I2080dfdc0a6aa5d9c18a245495ba72396367fb77

--- src/main/groovy/org/jruyi/gradle/thrift/plugin/ThriftPlugin.groovy
+++ src/main/groovy/org/jruyi/gradle/thrift/plugin/ThriftPlugin.groovy
@@ -20,68 +20,67 @@ import org.gradle.api.tasks.Copy
 
 class ThriftPlugin implements Plugin<Project> {
 
-	public static final String COMPILE_THRIFT_TASK = 'compileThrift'
-	public static final String CHECK_THRIFT_TASK = 'checkThrift'
+    public static final String COMPILE_THRIFT_TASK = 'compileThrift'
+    public static final String CHECK_THRIFT_TASK = 'checkThrift'
 
-	public static final String THRIFT_VERSION = '0.9'
+    public static final String THRIFT_VERSION = '0.9'
 
-	@Override
-	void apply(Project project) {
+    @Override
+    void apply(Project project) {
 
-		project.configurations {
-			thriftExecutable {
-				description = "Configuration used to get a thrift executable"
-				canBeResolved = true
-				canBeConsumed = false
-			}
-		}
+        project.configurations {
+            thriftExecutable {
+                description = "Configuration used to get a thrift executable"
+                canBeResolved = true
+                canBeConsumed = false
+                defaultDependencies { deps ->
+                    deps.add(project.dependencies.create("com.criteo.devtools:thrift:$THRIFT_VERSION") {
+                        artifact {
+                            name = "thrift"
+                            type = "tgz"
+                        }
+                    })
+                }
+            }
+        }
 
-		project.dependencies {
-            thriftExecutable("com.criteo.devtools:thrift:$THRIFT_VERSION") {
-				artifact {
-					name = "thrift"
-					type = "tgz"
-				}
-			}
-		}
+        project.repositories {
+            maven {
+                url 'http://build-nexus.crto.in/repository/criteo.thirdparty'
+                content {
+                    // this repository *only* contains artifacts with group "com.criteo.devtools" module "thrift"
+                    includeModule("com.criteo.devtools", "thrift")
+                }
+            }
+        }
 
-		project.repositories {
-			maven {
-				url 'http://build-nexus.crto.in/repository/criteo.thirdparty'
-				content {
-					// this repository *only* contains artifacts with group "com.criteo.devtools" module "thrift"
-					includeModule("com.criteo.devtools", "thrift")
-				}
-			}
-		}
+        def srcDir = "${project.projectDir}/src/main/thrift"
+        def dstDir = "${project.buildDir}/generated-sources/thrift"
 
-		def srcDir = "${project.projectDir}/src/main/thrift"
-		def dstDir = "${project.buildDir}/generated-sources/thrift"
+        CompileThrift compileThrift = project.tasks.create(COMPILE_THRIFT_TASK, CompileThrift)
+        compileThrift.sourceDir(srcDir)
+        compileThrift.outputDir(dstDir)
 
-		CompileThrift compileThrift = project.tasks.create(COMPILE_THRIFT_TASK, CompileThrift)
-		compileThrift.sourceDir(srcDir)
-		compileThrift.outputDir(dstDir)
+        def checkThriftTask = project.tasks.register(CHECK_THRIFT_TASK, Copy) {
+            from project.configurations.thriftExecutable.elements.map { project.tarTree(it.first()) }
+            into project.layout.buildDirectory.dir('thrift')
+        }
+        compileThrift.thriftBinaryFolder.set(project.layout.projectDirectory.dir(checkThriftTask.map { it.destinationDir.absolutePath }))
+        compileThrift.dependsOn(checkThriftTask)
 
-		def checkThriftTask = project.tasks.register(CHECK_THRIFT_TASK, Copy) {
-			from project.tarTree(project.configurations.thriftExecutable.singleFile)
-			into project.rootProject.layout.buildDirectory.dir('thrift')
-		}
-		compileThrift.thriftBinaryFolder.set(checkThriftTask.get().destinationDir)
-		compileThrift.dependsOn(checkThriftTask)
-
-		project.configure(project) {
-			sourceSets {
-				main {
-					resources {
-						srcDirs srcDir
-						// If the project don't already has includes, then all the resources will be included.
-						if (!includes.isEmpty()) {
-							// If the project already has includes, then enrich the include rules to include thrift files
-							include '**/*.thrift'
-						}
-					}
-				}
-			}
-		}
-	}
+        project.configure(project) {
+            sourceSets {
+                main {
+                    resources {
+                        srcDirs srcDir
+                        // If the project don't already has includes, then all the resources will be included.
+                        if (!includes.isEmpty()) {
+                            // If the project already has includes, then enrich the include rules to include thrift files
+                            include '**/*.thrift'
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
